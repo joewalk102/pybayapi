0edb628f0107757b3745f72b77fc2b92b1502df9
Joe Walker <joewalk102@gmail.com>
2020-12-06 11:13:14 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pybayapi',
+    version='0.1.0a',
+    packages=['pybay', 'pybay.buy', 'pybay.buy.browse'],
+    url='https://github.com/joewalk102/pybayapi',
+    license='',
+    author='joewalk102',
+    author_email='joewalk102@gmail.com',
+    description=''
+)
